c40218138174cdf1a31be3a6ecadd5488941d909
Michele Ribeiro <micheler720@gmail.com>
2021-02-15 18:51:06 -0300

    Alteraçao Rota home

--- Startup.cs
+++ Startup.cs
@@ -84,11 +84,11 @@ namespace api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
